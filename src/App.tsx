6a73f664de5a845227caac545ecc46052345618f
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-05-24 11:08:04 -0300

    App module create

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import styles from './App.module.css'
+import poweredImg from './assets/images/powered.png'
 
 const App = () => {
   return (
-    <div className={styles}>...</div>
+    <div className={styles.main}>
+
+      <header className={styles.headerContainer}>
+        <div>
+          <img src={poweredImg} alt="logo" />
+        </div>
+      </header>
+
+      <div className={styles.container}>
+
+        <div className={styles.leftSide}>
+          ...
+        </div>
+        <div className={styles.rightSide}>
+          ...
+        </div>
+
+      </div>
+    </div>
   )
 }
 
